9d51621c6d6fb630cb6205d9267ab0224055f33a
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-05-29 14:44:28 -0300

    Inclusao programa c#
    calculo de idade com erro no teste fechado

--- UltrapassandoV/Program.cs
+++ UltrapassandoV/Program.cs
@@ -2,11 +2,47 @@
 
 namespace UltrapassandoV
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+
+class MainClass {
+
+  public static void Main () {
+
+    var R = int32.Parse(Console.ReadLine());
+    var V = 0;
+    var b = 0;
+    var c = 0;
+    var d = 0;
+
+    do { 
+ 
+      V = int.Parse(Console.ReadLine());
+//      Console.WriteLine(" V " + V );
+
+    } while ( R >= V  );
+
+      b = R;
+      d = R;
+//      c++;
+//      Console.WriteLine(" b " + b );
+      
+    for ( int x = b ; x <= V ; x++    ) {
+           
+//      Console.WriteLine(" c " + c );     
+//      Console.WriteLine(" x " + x );    
+//      Console.WriteLine(" R " + R );
+      
+      if ( d > V ) {
+        break ;          
         }
+      else {
+        c++;
+        d = d + x ;  
+      }
+
     }
+    Console.WriteLine(c);
+    
+  }
+}
 }
---
+++ idades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace idades {
+
+    class Desafio    {
+        static void Main(string[] args)  {
+            int n = 1;
+            //declare suas variaveis corretamente
+            int Ct_Idade = 0;
+            int Ct_Count = 0;
+
+            while (   n >= 0   )  {
+                n = int.Parse(Console.ReadLine());
+
+                if ( n >= 0      )  {
+                     Ct_Idade+= n;
+                     Ct_Count++;
+                }
+            }
+
+            double  Ct_IdadeMedia     =  (double)Ct_Idade    /   Ct_Count   ;
+            
+            Console.WriteLine( "sum   " + Ct_Idade.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine( "count " + Ct_Count.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine( Ct_IdadeMedia.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
