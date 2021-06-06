c78f873371b1a5be8964e829e186e1767a04c72f
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-06-06 20:52:13 -0300

    inclusao de novos codigos
    Iniciando a programar em C#
    Dividindo X por Y
    Soma Simples
    Média 1

---
+++ Iniciando a programar em C#/SomaSimples.cs
@@ -0,0 +1,20 @@
+// Iniciando a programar em C#
+// Soma Simples
+
+using System;
+
+class Program{
+    static void Main(string[] args)    {
+
+        //Declare Variables
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+        
+        //Insira as variaveis de acordo com o solicitado
+        int SOMA =  A + B     ;
+
+        //Print
+        Console.WriteLine("SOMA = {0}", SOMA);
+
+    }
+}
